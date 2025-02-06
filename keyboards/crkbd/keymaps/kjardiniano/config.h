bb3e790aaa58d39fc2e24f0b4a32e0edfcd5e946
Keith <keithanselmo.jardiniano@gmail.com>
2025-02-06 19:38:33 +0800

    tapping term to 200ms

--- keyboards/crkbd/keymaps/kjardiniano/config.h
+++ keyboards/crkbd/keymaps/kjardiniano/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_MATRIX_I2C
 
 // #define QUICK_TAP_TERM 0
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 #define TAPPING_FORCE_HOLD_PER_KEY
 
